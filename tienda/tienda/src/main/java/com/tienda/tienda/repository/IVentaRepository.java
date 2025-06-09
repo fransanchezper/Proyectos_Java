bd5674da1515fee61fe8617a72336332c3da9f9a
Fran <fransanchezpe@gmail.com>
2025-06-09 09:40:45 +0200

    proyecto listo para probar

--- tienda/tienda/src/main/java/com/tienda/tienda/repository/IVentaRepository.java
+++ tienda/tienda/src/main/java/com/tienda/tienda/repository/IVentaRepository.java
@@ -18,6 +18,11 @@ public interface IVentaRepository extends JpaRepository<Venta, Long>{
      TuplaLongDouble getResumenVentasPorFecha(@Param("fecha_venta") LocalDate fecha_venta);
 
 
-    @Query(SELECT vd FROM VentasDTO vd WHERE)
+    @Query("SELECT v.codigo_venta, v.total, count(p.codigo_producto) as cantidad_producto, c.nombre, c.apellido\n" + //
+                "FROM Venta v \n" + //
+                "join Producto p on v.codigo_venta = p.codigo_venta\n" + //
+                "join Cliente c on v.id_client = c.id_client\n" + //
+                "where v.total = (SELECT max(total) FROM Venta) \n" + //
+                "group by v.codigo_venta limit 1;")
      VentasDTO getMayorVenta();
 }
